109e416aab10f83aa4142bce42ade980c2bfa81f
Rohit Menon <rohitsaggi88@gmail.com>
2016-07-26 23:23:20 -0700

    Models added for node and linkedlist

---
+++ src/main/java/com/rm/model/LinkedList.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.rm.model;
+
+/**
+ * @author rohitmenon
+ *
+ */
+public class LinkedList<T> {
+	
+	Node<T> head_;
+	
+	public LinkedList() {
+		head_ = null;
+	}
+
+	public LinkedList(Node<T> head) {
+		this.head_ = head;
+	}
+	
+	public Node<T> getHead() {
+		return head_;
+	}
+
+	public void setHead(Node<T> head) {
+		this.head_ = head;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/rm/model/Node.java
@@ -0,0 +1,35 @@
+package com.rm.model;
+
+/**
+ * 
+ * @author rohitmenon
+ *
+ * @param <T>
+ */
+public class Node<T> {
+
+	private T value_;
+	private Node<T> next_;
+
+	public T getValue() {
+		return value_;
+	}
+
+	public void setValue(T value) {
+		this.value_ = value;
+	}
+
+	public Node<T> getNext() {
+		return next_;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next_ = next;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [value=" + value_ + "]";
+	}
+
+}
